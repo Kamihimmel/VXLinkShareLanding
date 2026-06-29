914c8f18ec1d1c8a7f9d2b8bd7496eb78bea2c06
123EFD <erchinqq06@gmail.com>
2026-06-29 14:08:49 +0000

    fix: stabilize mobile web app viewport and cache

--- App/sw.js
+++ App/sw.js
@@ -1,7 +1,7 @@
-const CACHE = "vx-link-helper-app-v1";
+const CACHE = "vx-link-helper-app-v2-20260629-viewport";
 const ASSETS = [
-  "./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest",
-  "./icons/icon-192.png", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
+  "./", "./index.html", "./styles.css?v=20260629-viewport", "./app.js?v=20260629-viewport", "./manifest.webmanifest?v=20260629-viewport",
+  "./icons/icon-192.png?v=20260629-viewport", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -11,5 +11,9 @@ self.addEventListener("activate", (event) => {
 });
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
