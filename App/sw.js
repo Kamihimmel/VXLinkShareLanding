6becf0c4b1975250ba74aceaea307e4cdf29bd88
123EFD <erchinqq06@gmail.com>
2026-06-29 21:15:24 +0000

    fix: reduce mobile bottom gap

--- App/sw.js
+++ App/sw.js
@@ -1,7 +1,7 @@
-const CACHE = "vx-link-helper-app-v4-20260629-fixedgap-version";
+const CACHE = "vx-link-helper-app-v5-20260629-compactgap-version";
 const ASSETS = [
-  "./", "./index.html", "./styles.css?v=20260629-fixedgap-version", "./app.js?v=20260629-fixedgap-version", "./manifest.webmanifest?v=20260629-fixedgap-version",
-  "./icons/icon-192.png?v=20260629-fixedgap-version", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
+  "./", "./index.html", "./styles.css?v=20260629-compactgap-version", "./app.js?v=20260629-compactgap-version", "./manifest.webmanifest?v=20260629-compactgap-version",
+  "./icons/icon-192.png?v=20260629-compactgap-version", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
