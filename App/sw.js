a20d8b46bce682aeb9f608fdc3c678f9704d5a99
123EFD <erchinqq06@gmail.com>
2026-06-30 06:44:04 +0000

    fix: polish liquid tabs and hide page scrollbars

--- App/sw.js
+++ App/sw.js
@@ -1,7 +1,7 @@
-const CACHE = "vx-link-helper-app-v9-20260630-flow-liquid-tabs-version";
+const CACHE = "vx-link-helper-app-v10-20260630-liquid-tabs-polish-version";
 const ASSETS = [
-  "./", "./index.html", "./styles.css?v=20260630-flow-liquid-tabs-version", "./app.js?v=20260630-flow-liquid-tabs-version", "./manifest.webmanifest?v=20260630-flow-liquid-tabs-version",
-  "./icons/icon-192.png?v=20260630-flow-liquid-tabs-version", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
+  "./", "./index.html", "./styles.css?v=20260630-liquid-tabs-polish-version", "./app.js?v=20260630-liquid-tabs-polish-version", "./manifest.webmanifest?v=20260630-liquid-tabs-polish-version",
+  "./icons/icon-192.png?v=20260630-liquid-tabs-polish-version", "./icons/icon-512.png", "./icons/maskable-192.png", "./icons/maskable-512.png", "../icon128.png"
 ];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
